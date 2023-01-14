3e0f7421b027304bd821d7461afcd73a5476ead6
Cameron Abel <cameronabel@gmail.com>
2023-01-14 00:37:48 -0800

    working api dumps response to screen

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';	
 import './css/styles.css';
+import ExchangeRateService from './js/exchange-rate-service';
+
+function getRateData() {
+  ExchangeRateService.getRates()
+    .then(function(rateResponse) {
+      if (rateResponse instanceof Error) {
+        const errorMessage = `There was a problem accessing the rate data from ExchangeRate-API:
+        ${rateResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      document.querySelector('body').innerText = JSON.stringify(rateResponse);
+    })
+    .catch(function(error) {
+      document.querySelector('body').innerText = error;
+    });
+}
+
+getRateData();
