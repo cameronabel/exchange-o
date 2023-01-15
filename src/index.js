d0d66b730418be799697e35721cc834b6796a1ac
Cameron Abel <cameronabel@gmail.com>
2023-01-14 23:53:13 -0800

    calculation displays in both directions

--- src/index.js
+++ src/index.js
@@ -1,21 +1,81 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';	
 import './css/styles.css';
-//import ExchangeRateService from './js/exchange-rate-service';
+import ExchangeRateService from './js/exchange-rate-service';
 
-// function getRateData() {
-//   ExchangeRateService.getRates()
-//     .then(function(rateResponse) {
-//       if (rateResponse instanceof Error) {
-//         const errorMessage = `There was a problem accessing the rate data from ExchangeRate-API:
-//         ${rateResponse.message}`;
-//         throw new Error(errorMessage);
-//       }
-//       document.querySelector('body').innerText = JSON.stringify(rateResponse);
-//     })
-//     .catch(function(error) {
-//       document.querySelector('body').innerText = error;
-//     });
-// }
 
-//getRateData();
+function getRateData() {
+  ExchangeRateService.getRates()
+    .then(function(rateResponse) {
+      if (rateResponse instanceof Error) {
+        const errorMessage = `There was a problem accessing the rate data from ExchangeRate-API:
+        ${rateResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      sessionStorage.clear();
+      sessionStorage.rateData = JSON.stringify(rateResponse);
+      sessionStorage.status = 'loaded';
+    })
+    .then(updateValueHandler)
+    .catch(function(error) {
+      document.getElementById('error-zone').innerText = error.message;
+    });
+}
+
+function getCurrencyData() {
+  ExchangeRateService.getCurrencies()
+    .then(function(currencyResponse) {
+      if (currencyResponse instanceof Error) {
+        const errorMessage = `There was a problem accessing the rate data from ExchangeRate-API:
+        ${currencyResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      document.getElementById('currencies').removeEventListener("click", getCurrencyData);
+      updateCurrencies(currencyResponse.supported_codes);
+    })
+    .catch(function(error) {
+      document.getElementById('error-zone').innerText = error.message;
+    });
+}
+
+function updateValueHandler() {
+  let outputField;
+  if (document.activeElement.id === 'usd-input') {
+    outputField = document.getElementById('other-input');
+  } else if (document.activeElement.id === 'other-input') {
+    outputField = document.getElementById('usd-input');
+  }
+  if (!sessionStorage.status) {
+    sessionStorage.status = 'pending';
+    getRateData();
+  } else if (sessionStorage.status === 'loaded'){
+    const rateData = JSON.parse(sessionStorage.rateData);
+    const currencyCode = document.getElementById('currencies').value || 'USD';
+    if (document.activeElement.id === 'usd-input') {
+      outputField.value = (document.activeElement.value * parseFloat(rateData.conversion_rates[currencyCode])).toFixed(2);
+    } else {
+      outputField.value = (document.activeElement.value / parseFloat(rateData.conversion_rates[currencyCode])).toFixed(2);
+    }
+  }  
+}
+
+function updateCurrencies(codes) {
+  codes.forEach(element => {
+    const [code, name] = element;
+    if (code !== 'USD') {
+      const option = document.createElement('option');
+      option.value = code;
+      option.innerText = `${code} - ${name}`
+      document.getElementById('currencies').append(option);
+    }
+  });
+}
+
+document.getElementById('usd-input').addEventListener("input", updateValueHandler);
+document.getElementById('other-input').addEventListener("input", updateValueHandler);
+document.getElementById('currencies').addEventListener("click", getCurrencyData);
+document.getElementById('currencies').addEventListener("change", function () {
+  document.getElementById('usd-input').focus();
+  updateValueHandler();
+  document.activeElement.blur();
+});
--- src/js/exchange-rate-service.js
+++ src/js/exchange-rate-service.js
@@ -13,4 +13,18 @@ export default class ExchangeRateService {
         return error;
       });
   }
+  static getCurrencies() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/codes`)
+      .then(function(response) {
+        if(!response.ok) {
+          const errorMessage = `${response.result} ${response['error-type']}`;
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }
+      })
+      .catch(function(error) {
+        return error;
+      })
+  }
 }
